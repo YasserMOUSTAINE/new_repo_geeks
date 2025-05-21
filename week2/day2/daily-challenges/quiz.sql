d0eaaa7d9ba5cf948a3157c7b8ea39c7446d3a3c
YasserMOUSTAINE <108281199+YasserMOUSTAINE@users.noreply.github.com>
2025-05-21 16:18:09 +0100

    Update quiz.sql

--- week2/day2/daily-challenges/quiz.sql
+++ week2/day2/daily-challenges/quiz.sql
@@ -1,8 +1,21 @@
-   SELECT COUNT(*) 
-    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
-	SELECT COUNT(*) 
-    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
-    SELECT COUNT(*) 
-    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
-    SELECT COUNT(*) 
-    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+SELECT COUNT(*) 
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
+-- Resultat : 0
+
+SELECT COUNT(*) 
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
+-- Resultat : 2
+
+
+SELECT COUNT(*) 
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
+-- Resultat : 0
+
+
+SELECT COUNT(*) 
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
+-- Resultat : 2
